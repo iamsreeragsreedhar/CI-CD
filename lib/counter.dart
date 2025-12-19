e9eb9af07f658cfabf0080e90b587537bbb3c65a
iamsreeragsreedhar <iamsreeragsreedhar@gmail.com>
2025-12-19 11:43:49 +0530

    chnegs added

---
+++ lib/counter.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text('Added test for checking the test of ci/cd pipeline'),
+            const Text('Added test for checking the test of ci/cd pipeline'),
+            const Text('Added test for checking the test of ci/cd pipeline'),
+            const Text('You have pushed the button for change in git '),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
